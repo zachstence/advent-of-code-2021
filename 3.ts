ccf570d011e8039b3c52d21d86fa39570a3b13ed
Zachary Stence <zachstence@gmail.com>
2021-12-02 23:22:07 -0600

    feat: solve 3

--- 3.ts
+++ 3.ts
@@ -1,1 +1,30 @@
-console.log("3");
+import input from "./3.json";
+
+let gamma = "";
+let epsilon = "";
+
+for (let i = 0; i < input[0].length; i++) {
+    let zeroes = 0;
+    let ones = 0;
+
+    for (const binary of input) {
+        if (binary.charAt(i) === "0") {
+            zeroes++;
+        } else {
+            ones++;
+        }
+    }
+
+    if (zeroes > ones) {
+        gamma += "0";
+        epsilon += "1";
+    } else {
+        gamma += "1";
+        epsilon += "0";
+    }
+}
+
+const gammaNum = parseInt(gamma, 2);
+const epsilonNum = parseInt(epsilon, 2);
+
+console.log(`Power Consumption: ${gammaNum * epsilonNum}`);
